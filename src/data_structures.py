abbacc1802545cedf7eea6548f0f2cd8e9c3a2f6
mSimon12 <marcelo_simon@hotmail.com>
2025-01-28 22:41:45 +0100

    feat: implement queue and stack

--- src/data_structures.py
+++ src/data_structures.py
@@ -6,20 +6,21 @@ class Node:
         self.data = data
         self.next = next_node
 
+
 class LinkedList:
     """
         Linked List class
     """
 
     def __init__(self):
-        self.head = None
+        self.__head = None
 
     def is_empty(self):
         """
             Verifies if the List is empty
         :return: emptiness status
         """
-        if self.head is None:
+        if self.__head is None:
             return True
 
         return False
@@ -29,7 +30,10 @@ class LinkedList:
             Retrieve the last node from the list and his parent node
         :return: last_node, parent_from_last_node
         """
-        current_node = self.head
+        if self.is_empty():
+            return self.__head, None
+
+        current_node = self.__head
         parent_node = current_node
 
         while current_node.next is not None:
@@ -48,7 +52,7 @@ class LinkedList:
             raise IndexError("Index out of range!")
 
         count = 0
-        current_node = self.head
+        current_node = self.__head
         parent_node = current_node
 
         while count != index:
@@ -75,8 +79,8 @@ class LinkedList:
         :param new_value: value to be added to the list
         :return:
         """
-        new_node = Node(new_value, self.head)
-        self.head = new_node
+        new_node = Node(new_value, self.__head)
+        self.__head = new_node
 
     def add_at_tail(self, new_value):
         """
@@ -84,8 +88,11 @@ class LinkedList:
         :param new_value: value to be added to the list
         :return:
         """
-        last_node, *_ = self.__get_last_node_and_parent()
-        last_node.next = Node(new_value, None)
+        if self.is_empty():
+            self.__head = Node(new_value, None)
+        else:
+            last_node, *_ = self.__get_last_node_and_parent()
+            last_node.next = Node(new_value, None)
 
     def add_at_index(self, new_value, index):
         """
@@ -94,10 +101,27 @@ class LinkedList:
         :param index: position to add the new item
         :return:
         """
+        if self.is_empty():
+            raise IndexError
+
         index_node, parent_node = self.__get_index_node_and_parent(index)
         parent_node.next = Node(new_value, index_node)
 
-    def delete_at_end(self):
+    def delete_from_head(self):
+        """
+            Remove the first item from the list
+        :param index: position to remove the new item
+        :return: value from the removed item
+        """
+        if self.is_empty():
+            raise IndexError("Delete from empty list!")
+
+        item_to_delete_data = self.__head.data
+        self.__head = self.__head.next
+
+        return item_to_delete_data
+
+    def delete_from_end(self):
         """
             Remove the last item from the list
         :return: value from the removed item
@@ -106,12 +130,12 @@ class LinkedList:
             raise IndexError("Delete from empty list!")
 
         last_node, parent_node = self.__get_last_node_and_parent()
-        popped_data = last_node.data
+        item_to_delete_data = last_node.data
 
         parent_node.next = None
-        return popped_data
+        return item_to_delete_data
 
-    def delete_at_index(self, index):
+    def delete_from_index(self, index):
         """
             Remove the item from the list at index position
         :param index: position to remove the new item
@@ -123,17 +147,50 @@ class LinkedList:
         index_node, parent_node = self.__get_index_node_and_parent(index)
         parent_node.next = index_node.next
 
+        return index_node.data
+
+    def __str__(self):
+        current_node = self.__head
+
+        index = 0
+        list_string = "index\tvalue"
+        while current_node.next is not None:
+            list_string += f"\n{index:5}\t{current_node.data}"
+            current_node = current_node.next
+            index += 1
+        list_string += f"\n{index:5}\t{current_node.data}"
+
+        return list_string
+
 
 class Queue:
 
     def __init__(self):
-        pass
+        self.__queue_data = LinkedList()
+
+    def add(self, new_queue_value):
+        self.__queue_data.add_at_tail(new_queue_value)
+
+    def remove(self):
+        return self.__queue_data.delete_from_head()
+
+    def __str__(self):
+        return self.__queue_data.__str__()
 
 
 class Stack:
 
     def __init__(self):
-        pass
+        self.__queue_data = LinkedList()
+
+    def add(self, new_queue_value):
+        self.__queue_data.add_at_head(new_queue_value)
+
+    def remove(self):
+        return self.__queue_data.delete_from_head()
+
+    def __str__(self):
+        return self.__queue_data.__str__()
 
 
 class Tree:
@@ -143,8 +200,21 @@ class Tree:
 
 
 if __name__ == "__main__":
-    # test_list = []
-    # test_list[0]
-    # test_list.pop()
-
     my_list = LinkedList()
+
+    def add_items_to_data_structure(data_structure):
+        items_to_add = [1, "a", 50.6, True]
+        for item in items_to_add:
+            data_structure.add(item)
+
+        print(data_structure)
+        print(type(data_structure))
+
+    queue_example = Queue()
+    stack_example = Stack()
+
+    print("Queue implementation example:")
+    add_items_to_data_structure(queue_example)
+
+    print("\nStack implementation example:")
+    add_items_to_data_structure(stack_example)
--- tests/dummy_test.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-class Test:
-
-    def test_dummy(self):
-        assert 1 == 1
