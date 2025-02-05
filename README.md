0201b2649ef24793c9356db1e5cc2a62786dfcae
mSimon12 <marcelo_simon@hotmail.com>
2025-02-05 17:49:35 +0100

    feat: memory performance in explained in readme

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
 # Python Algorithms
 
 Performance processing an application is always welcome.
-Therefore, here we include a set of data structures and algorithms implemented to 
+Therefore, here I include a set of data structures and algorithms implemented to 
 execute their task in the most efficient way.
-```
+
+## Data Structures
+Data structures are fundamental components of computer science that help in organizing,
+storing, and managing data efficiently. 
+They enable optimal data manipulation, retrieval, and storage, improving the performance of algorithms. 
+Common data structures include arrays, linked lists, stacks, queues, trees, 
+and graphs, each suited for specific tasks. 
+I can consider the following table to do the best choice when deciding which one to use.
+
+| **Data Structure**    | **Access Time** | **Search Time** | **Insertion/Deletion** | **Memory Usage** | **Best Use Cases** |
+|-----------------------|----------------|-----------------|------------------------|------------------|---------------------|
+| **Array**            | O(1)            | O(n)            | O(n) (for shifting)    | Fixed size       | Static data, fast index access |
+| **Linked List**      | O(n)            | O(n)            | O(1) (at head/tail)    | Dynamic size     | Frequent insertions/deletions |
+| **Stack**           | O(n)            | O(n)            | O(1) (push/pop)        | Dynamic size     | Backtracking, function calls |
+| **Queue**           | O(n)            | O(n)            | O(1) (enqueue/dequeue) | Dynamic size     | Scheduling, breadth-first search |
+| **Hash Table / Dict** | O(1) (average) | O(1) (average)  | O(1) (average)        | High (depends on load factor) | Fast lookups, key-value mapping (e.g., Python `dict`) |
+| **Binary Tree**      | O(log n)        | O(log n)        | O(log n) (balanced)   | Dynamic size     | Hierarchical data, search trees |
+
+In this repository I have implemented Linked List, Queue, Stack and BinaryTree which are introduced at
+[Data Structures](src/data_structures). To verify the specifics of each type, I have accomplished a set of 
+test to measure memory usage and performance in different conditions.
+
+### Memory
+When we put different data structures holding the same data we can verify the difference memory needed
+by each one. I have compared the memory needed by [List, Dictionary, Linked List, Queue, Stack and Binary Tree]
+to hold 400 random data values. These values were added one by one and the memory after each inclusion
+was measured.
+
+The result shows continuous increase in the memory for the types I have implements here, and continuous increase
+followed by periodical resize of the data chunks for the List and the Dictionary. The memory required by 
+the new structures is 4 to 5 times bigger than the needed for a List, due to the implementation of a Node
+class that is used in the dynamic allocation of the data.
+
+<picture>
+ <img alt="Comparison of memory usage of data structures" src="src/outputs/basic_memory_usage.png" width="600">
+</picture>
+
+**Arrays** generally offer better memory efficiency for small datasets due to their contiguous storage, 
+minimal overhead, and superior cache locality, making them ideal for fast random access. However, 
+they suffer from resizing overhead and fragmentation issues when dynamically growing. 
+In contrast, **Linked Lists** provide flexible memory allocation and efficient insertions/deletions without 
+resizing, making them preferable for frequently changing data structures. Despite this advantage, 
+linked lists have higher memory overhead due to pointers and suffer from poor cache locality.
--- src/performance.py
+++ src/performance.py
@@ -1,44 +1,51 @@
 import sys
+import os
 import timeit
 import random
 import pandas as pd
 from pympler import asizeof
 from matplotlib import pyplot as plt
 
-from data_structures.basic_data_structures import LinkedList, Queue, Stack
+from data_structures.basic_data_structures import LinkedList, Queue, Stack, Node
 from data_structures.binary_tree import BinaryTree
 
 
-class PerformanceAnalyzer:
+class MemoryAnalyzer:
 
     def __init__(self):
         self.__list = []
+        self.__dict = {}
         self.__linked_list = LinkedList()
         self.__queue = Queue()
         self.__stack = Stack()
+        self.__tree = BinaryTree()
 
         self.values_for_test = random.sample(range(10000), 400)
-        self.__memory_usage = pd.DataFrame(columns=['list', 'linked_list', 'queue', 'stack'])
-
+        self.__memory_usage = pd.DataFrame(columns=['list', 'dict', 'linked_list', 'queue', 'stack', 'tree'])
 
     def __measure_memory_sizes(self):
         memory_sizes = {
             'list': asizeof.asizeof(self.__list, limit=10000, detail=True),
+            'dict': asizeof.asizeof(self.__dict, limit=10000, detail=True),
             'linked_list': asizeof.asizeof(self.__linked_list, limit=10000, detail=True),
             'queue': asizeof.asizeof(self.__queue, limit=10000, detail=True),
-            'stack': asizeof.asizeof(self.__stack, limit=10000, detail=True)
+            'stack': asizeof.asizeof(self.__stack, limit=10000, detail=True),
+            'tree': asizeof.asizeof(self.__tree, limit=10000, detail=True)
         }
         return memory_sizes
 
-    def analyze_memory_usage(self):
+    def gather_memory_usage(self):
         self.__memory_usage.loc[0, :] = self.__measure_memory_sizes()
 
         counter = 1
         for test_value in self.values_for_test:
             self.__list.append(test_value)
+            self.__dict[counter] = None
+
             self.__linked_list.add_at_tail(test_value)
             self.__queue.add(test_value)
             self.__stack.add(test_value)
+            self.__tree.insert_node(counter, None)
 
             self.__memory_usage.loc[counter, :] = self.__measure_memory_sizes()
             counter += 1
@@ -46,25 +53,40 @@ class PerformanceAnalyzer:
         return self.__memory_usage.copy()
 
     def plot_memory_usage(self):
+        plt.style.use("bmh")
+
         fig, axes = plt.subplots(1, 1)
         self.__memory_usage.plot(ax= axes)
 
-        axes.set_xlabel("Values added (n)", fontsize=14)
+        axes.set_xlabel("Data added to Data Structure(n)", fontsize=14)
         axes.set_ylabel("Memory usage (Bytes)", fontsize=14)
         axes.set_xlim(0, 400)
-        axes.grid()
+        axes.grid(True)
         plt.locator_params(axis='y', nbins=20)
-        plt.locator_params(axis='x', nbins=40)
-        fig.suptitle("Data Structures memory usage (Bytes)", fontsize=16, fontweight='bold')
+        plt.locator_params(axis='x', nbins=15)
+        fig.suptitle("Data Structures memory usage", fontsize=16, fontweight='bold')
+        fig.tight_layout()
+
+
+class Analyzer:
+    OUTPUT_DIR = "outputs"
+
+    def __init__(self):
+        self.__prepare_environment()
+
+    def __prepare_environment(self):
+        if not os.path.isdir(self.OUTPUT_DIR):
+            os.mkdir(self.OUTPUT_DIR)
+
+    def evaluate_memory(self):
+        print("Memory evaluation!")
+        memory_checker = MemoryAnalyzer()
+        memory_checker.gather_memory_usage()
+        memory_checker.plot_memory_usage()
+
+        plt.savefig(self.OUTPUT_DIR + "/basic_memory_usage.png")
 
 
 if __name__ == "__main__":
-    print("Performance evaluation!")
-
-    performance_checker = PerformanceAnalyzer()
-    performance_checker.analyze_memory_usage()
-    performance_checker.plot_memory_usage()
-
-    plt.show()
-
-
+    analyzer = Analyzer()
+    analyzer.evaluate_memory()
