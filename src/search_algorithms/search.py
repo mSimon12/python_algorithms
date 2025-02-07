7bd7d128238defff4c69d13fca06acd2b944f8e4
mSimon12 <marcelo_simon@hotmail.com>
2025-02-07 20:51:29 +0100

    feat: linear search and test done

---
+++ src/search_algorithms/search.py
@@ -0,0 +1,49 @@
+import random
+
+class Searcher:
+
+    def __init__(self, dataset= None):
+        self.__dataset = dataset
+
+    @property
+    def dataset(self):
+        return self.__dataset
+
+    @dataset.setter
+    def dataset(self, search_list):
+        self.__dataset = search_list
+
+    def linear_search(self, searched_value):
+        """
+            Implements linear search, iterating over each element until find match
+        :param searched_value: value to be looked in the list
+        :return: index of match or None
+        """
+        if self.__dataset is None:
+            raise ValueError("Search list was not set!")
+
+        count = 0
+        for next_value in self.__dataset:
+            if searched_value == next_value:
+                return count
+            count += 1
+
+        return None
+
+
+if __name__ == "__main__":
+    data = random.sample(range(10000), 10)
+
+    my_searcher = Searcher()
+    my_searcher.dataset = data
+
+    interest_value = int(input("Inform value to search: "))
+    value_idx = my_searcher.linear_search(interest_value)
+
+    if value_idx is not None:
+        print(f"The value '{interest_value} is at index {value_idx}")
+    else:
+        print("The searched value is not in the list!")
+
+
+
---
+++ tests/search_algorithms/search_test.py
@@ -0,0 +1,24 @@
+import pytest
+from src.search_algorithms.search import Searcher
+
+class TestSearch:
+
+    @pytest.fixture()
+    def test_searcher(self):
+        return Searcher([-53, 120.9, "a", 1000])
+
+    def test_linear_search_value_present(self, test_searcher):
+        assert 0 == test_searcher.linear_search(test_searcher.dataset[0]), "Fail to find value index in list!"
+        assert 1 == test_searcher.linear_search(test_searcher.dataset[1]), "Fail to find value index in list!"
+        assert 2 == test_searcher.linear_search(test_searcher.dataset[2]), "Fail to find value index in list!"
+        assert 3 == test_searcher.linear_search(test_searcher.dataset[3]), "Fail to find value index in list!"
+
+    def test_linear_search_invalid_value(self, test_searcher):
+        assert None == test_searcher.linear_search(0), "Fail, value not in list!"
+        assert None == test_searcher.linear_search(10), "Fail, value not in list!"
+        assert None == test_searcher.linear_search(500), "Fail, value not in list!"
+
+    def test_linear_search_no_data_exception(self):
+        test_searcher = Searcher()
+        with pytest.raises(ValueError):
+            test_searcher.linear_search(0)
